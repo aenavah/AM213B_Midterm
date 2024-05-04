dec42442f38dfbf666dcdd4aa852f8eccf3cf23d
Alexandra Nava <aenava@ucsc.edu>
2024-05-04 00:56:11 -0700

    q2a, q2b, 3a, 3b

--- Midterm_Driver.py
+++ Midterm_Driver.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 
 import Midterm_functions
 plot_boundary = Midterm_functions.plot_boundary 
+RK3_AbsoluteStabilityRegion = Midterm_functions.RK3_AbsoluteStabilityRegion
 
-
-### Question 3b : Plot the region of Stability
+'''
+Question 3b : Plot the region of Stability
+'''
 def LMM_Boundary(theta):
   '''LMM method from Q3 boundary'''
   y = 4*(-1 - np.e**(1j*theta) - np.e**(2j*theta) + 3*np.e**(3j*theta))/(3 - 2*np.e**(1j*theta) + 23*np.e**(2j*theta))
@@ -14,3 +16,25 @@ def LMM_Boundary(theta):
 plot_boundary(LMM_Boundary)
 plt.title("LMM (7) Absolute Stability Region")
 plt.savefig("Q3b_LMM_AbsoluteStabilityRegion.jpg")
+plt.clf()
+
+'''
+Question 2b : Plot the region of absolute stability of the RK3 method
+'''
+A = np.array([[0, 0, 0],
+              [1/4, 1/4, 0],
+              [0, 1, 0]])
+b = np.array(([1/6],[2/3],[1/6]))
+h = np.ones_like(b)
+
+
+RK3_AbsoluteStabilityRegion(A, b, h)
+
+'''
+ Question 2c: Determine the largest delta t for which the RK3 applied to dy/dt = By
+'''
+B = np.array([-1, 3, -5, 7],
+             [0, -2, 4, -6],
+             [0, 0, -4, 6],
+             [0, 0, 0, -16])
+
--- Midterm_functions.py
+++ Midterm_functions.py
@@ -25,3 +25,28 @@ def plot_boundary(method):
   plt.xlabel("Re(z)")
   plt.ylabel("Im(z)")
   plt.plot(real_vals, imag_vals)  
+
+def RK3_S(A, b, h, x, y):
+  z = complex(x,y)
+  S = abs(np.linalg.det(np.eye(3) - (z*A) + z*(h@(b.T))))
+  return S
+
+def RK3_AbsoluteStabilityRegion(A, b, h):
+  nx, ny = (200, 200)
+  imag = np.linspace(-4, 4, nx)
+  real = np.linspace(-6, 2, ny)
+  real_v, imag_v = np.meshgrid(real, imag)
+
+  S_grid = np.ones((nx, ny))
+
+  for x_i in range(nx):
+    for y_i in range(ny):
+      boundary = RK3_S(A, b, h, real_v[x_i, y_i], imag_v[x_i, y_i])
+      S_grid[x_i, y_i] = boundary 
+
+  plt.xlabel("Re(z)")
+  plt.ylabel("Im(z)")
+  plt.contour(real_v, imag_v, S_grid, [1])
+  plt.grid()
+  plt.title("Boundary of RK3 Method")
+  plt.savefig("Q2b_RK3_Boundary.jpg")
